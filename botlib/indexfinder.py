bb7a01521b4d3009e2ef860292d85a47a799cdb5
Mantas <sirexas@gmail.com>
2015-10-26 18:42:09 +0200

    Pattern finder (work in progress)

--- botlib/indexfinder.py
+++ botlib/indexfinder.py
@@ -2,10 +2,12 @@ import re
 import pathlib
 import collections
 import argparse
+import itertools
 
 from databot.commands import CommandsManager, Command
+from botlib.combinations import combinations, strjoin
 
-pattern_re = re.compile(r'{[^}]+}', re.UNICODE)
+pattern_re = re.compile(r'({[^}]+})', re.UNICODE)
 norm_re = re.compile(r'\W+', re.UNICODE)
 
 
@@ -35,7 +37,7 @@ class IndexFinder(object):
                 for alias in aliases:
                     if '{' in alias:
                         try:
-                            patterns.append(self.create_regex(alias))
+                            patterns.append(self.parse_patterns(alias))
                         except re.error as e:
                             raise ValueError('\n'.join([
                                 "Error while parsing %r index alias:" % name,
@@ -45,33 +47,119 @@ class IndexFinder(object):
                     else:
                         index_aliases[name][norm(alias)] = choice
                 if patterns:
-                    index_patterns[name].append((self.create_replacement_template(choice), patterns))
+                    index_patterns[name].append((choice, patterns))
         return index_aliases, index_patterns
 
     def parse_expr(self, expr):
         name, flags = expr.split(':', 1) if ':' in expr else (expr, '')
-        flags = flags.split(',')
+        flags = tuple(filter(None, map(str.strip, flags.split(','))))
         return name, flags
 
-    def create_regex(self, value):
-        pattern = value
-        for place_holder in pattern_re.findall(value):
-            name, flags = self.parse_expr(place_holder[1:-1])
-            pattern = pattern.replace(place_holder, r'\b(?P<%s>\w+)\b' % name)
-        return re.compile('^%s$' % pattern, re.UNICODE | re.IGNORECASE)
-
-    def create_replacement_template(self, value):
-        tempalte = value
-        for place_holder in pattern_re.findall(value):
-            name, flags = self.parse_expr(place_holder[1:-1])
-            tempalte = tempalte.replace(place_holder, r'\g<%s>' % name)
-        return tempalte
+    def parse_patterns(self, value):
+        result = []
+        for token in pattern_re.split(value):
+            token = token.strip()
+            if token.startswith('{'):
+                name, flags = self.parse_expr(token[1:-1])
+                result.append((name, flags))
+            elif token:
+                result.append(token)
+        return result
 
     def check_groups(self, match):
         for name, value in match.groupdict().items():
             if value not in self.index[name]:
                 return False
         return True
+
+    def pattern_finder(self, patterns, value):
+        """Search for all possible matches for given pattern and value.
+
+        How does this work.
+
+        For example you have these indexes defined:
+
+            company-type/
+                aliases.txt:
+                    UAB
+                        Uždaroji akcinė bendrovė
+                    IĮ
+                        Individuali įmonė
+                choices.txt:
+                    1,UAB
+                    2,IĮ
+            company/
+                aliases.txt:
+                    {company}
+                        {company-type} {company}
+                    Programuotojų artelė
+                        Programmers of Vilnius
+                choices.txt:
+                    1,Programuotojų artelė
+
+        Then you call:
+
+            pattern_finder([('company-type', ()), ('company', ())], 'uždaroji akcinė bendrovė programmers of vilnius')
+
+        Finder will collect all posible combinations for 'uždaroji akcinė bendrovė programmers of vilnius' and two
+        patterns:
+
+            ('uždaroji', 'akcinė bendrovė programmers of vilnius')
+            ('uždaroji akcinė', 'bendrovė programmers of vilnius')
+            ('uždaroji akcinė bendrovė', 'programmers of vilnius')
+            ('uždaroji akcinė bendrovė programmers', 'of vilnius')
+            ('uždaroji akcinė bendrovė programmers of', 'vilnius')
+
+        Then for each conbination you collect possible pattern choices, first for raw strings that match. In our case
+        there is not raw strings, only two patterns, so nothing will happen here.
+
+        Then collect all possible choices for given patterns, by searching index specified in each pattern for value
+        from generated combinations. In our case, only 'uždaroji akcinė bendrovė' will give 'uab' and 'programmers of
+        vilnius' will give 'programuotojų artelė', all other combination values will not return any results.
+
+            [['uab'], ['programuotojų artelė']]
+
+        And finally generate all possible combinations:
+
+            {'company-type': 'uab', 'company': 'programuotojų artelė'}
+
+        Arguments:
+        - patterns: list, example: [('bank', ()), 'bankas']
+        - value: str, normalized value (see norm), example: 'dnb bankas'
+
+        Returns generator with all possible values.
+        """
+        n_patterns = len(patterns)
+        choices = [[] for i in range(n_patterns)]
+
+        for comb in strjoin(combinations(n_patterns, value)):
+            skip = False
+
+            # First check all raw strings, if at least one raw string does not match, skip.
+            for i, (token, pattern) in enumerate(zip(comb, patterns)):
+                if token == pattern:
+                    choices[i].append(token)
+                elif isinstance(pattern, str):
+                    skip = True
+                    break
+            if skip:
+                continue
+
+            # Find all indexes.
+            for i, (token, pattern) in enumerate(zip(comb, patterns)):
+                print(token, pattern)
+                if isinstance(pattern, tuple):
+                    appended = False
+                    name, flags = pattern
+                    for id, _value in self.find(name, token):
+                        choices[i].append(norm(_value))
+                        appended = True
+                    if not appended:
+                        break
+
+        # Finally generate all possible combinations from found indexes and matching raw strings.
+        for option in itertools.product(*choices):
+            yield {k[0]: v for k, v in zip(patterns, option) if isinstance(k, tuple)}
 
     def find(self, name, value):
         idx = self.index[name]
@@ -84,15 +172,11 @@ class IndexFinder(object):
             _value = self.aliases[name][value]
             yield idx.get(norm(_value), (None, _value))
 
+        value = value.split()
         for replacement, patterns in self.patterns[name]:
-            for regex in patterns:
-                match = regex.search(value)
-                if match:
-                    for name, value in match.groupdict().items():
-                        if value not in self.index[name]:
-                            return False
-
-                    _value = match.expand(replacement)
+            for pattern in patterns:
+                for groups in self.pattern_finder(patterns, value):
+                    _value = replacement.format(**groups)
                     yield idx.get(norm(_value), (None, _value))
 
 
--- tests/test_indexfinder.py
+++ tests/test_indexfinder.py
@@ -15,7 +15,24 @@ class LoadIndexTests(unittest.TestCase):
                     (3, 'seb'),
                     (4, 'swedbank'),
                 ],
-                'aliases': [],
+                'aliases': [
+                    ('{bank}', [
+                        '{bank} bankas',
+                        '{bank} bank',
+                        'bankas {bank}',
+                        '{bank:genitive} lizingas',
+                    ]),
+                ],
+            },
+            'company-type': {
+                'index': [
+                    (1, 'UAB'),
+                    (2, 'IĮ'),
+                ],
+                'aliases': [
+                    ('UAB', ['Uždaroji akcinė bendrovė']),
+                    ('IĮ', ['Individuali įmonė']),
+                ],
             },
             'company': {
                 'index': [
@@ -23,14 +40,12 @@ class LoadIndexTests(unittest.TestCase):
                     (2, 'dnb bankas'),
                     (3, 'seb bankas'),
                     (4, 'swedbank bankas'),
+                    (5, 'programuotojų artelė'),
                 ],
                 'aliases': [
-                    ('{bank} bankas', [
-                        '{bank}',
-                        '{bank} bank',
-                        'bankas {bank}',
-                        '{bank:genitive} lizingas',
-                    ])
+                    ('{bank} bankas', ['{bank}']),
+                    ('{company}', ['{company-type} {company}']),
+                    ('Programuotojų artelė', ['Programmers of Vilnius']),
                 ],
             },
         })
@@ -44,14 +59,20 @@ class IndexFinderTests(unittest.TestCase):
     def assertIndex(self, index, value, result):
         self.assertEqual(list(self.index.find(index, value)), result)
 
-    def test_create_regex(self):
-        self.assertEqual(self.index.create_regex('bankas {company}').pattern, r'^bankas \b(?P<company>\w+)\b$')
-
-    def test_create_replacement_template(self):
-        self.assertEqual(self.index.create_replacement_template('bankas {company}'), r'bankas \g<company>')
+    def test_parse_patterns(self):
+        self.assertEqual(self.index.parse_patterns('bankas {company}'), ['bankas', ('company', ())])
 
     def test_parse_expr(self):
-        self.assertEqual(self.index.parse_expr('company:genitive'), ('company', ['genitive']))
+        self.assertEqual(self.index.parse_expr('company'), ('company', ()))
+        self.assertEqual(self.index.parse_expr('company:genitive'), ('company', ('genitive',)))
+
+    def test_patterns(self):
+        self.index = IndexFinder(load_index('tests/fixtures/index'))
+        pattern = [('company-type', ()), ('company', ())]
+        value = 'uždaroji akcinė bendrovė programmers of vilnius'
+        self.assertEqual(list(self.index.pattern_finder(pattern, value.split())), [
+            {'company-type': 'uab', 'company': 'programuotojų artelė'},
+        ])
 
     def test_find(self):
         self.index = IndexFinder(load_index('tests/fixtures/index'))
