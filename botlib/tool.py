a536a33eded711b4fa58db31d8b8461e24df4a4b
Mantas <sirexas@gmail.com>
2015-11-17 10:38:04 +0200

    Better data syncing tui

--- botlib/tool.py
+++ botlib/tool.py
@@ -8,14 +8,35 @@ def call(cmd):
     subprocess.check_call(cmd)
 
 
+def _rsync(args):
+    delete = '--delete-after' if args.delete else ''
+    dry_run = '--dry-run' if args.dry_run else ''
+    exclude = ['--exclude=%s' % pattern for pattern in ['.ipynb_checkpoints/']]
+    return ['rsync', dry_run, '-avi', '--human-readable', '--progress', delete] + exclude
+
+
 def push(args):
-    delete = '--delete' if args.delete else ''
-    call(['rsync', '-av', delete, 'data/', 'atviriduomenys.lt:/opt/atviriduomenys.lt/app/var/www/data'])
+    source = 'data/'
+    target = 'atviriduomenys.lt:/opt/atviriduomenys.lt/app/var/www/data'
+    call(_rsync(args) + [source, target])
 
 
 def pull(args):
-    delete = '--delete' if args.delete else ''
-    call(['rsync', '-av', delete, 'atviriduomenys.lt:/opt/atviriduomenys.lt/app/var/www/data/', 'data'])
+    source = 'atviriduomenys.lt:/opt/atviriduomenys.lt/app/var/www/data/'
+    target = 'data'
+    call(_rsync(args) + [source, target])
+
+
+def put(args):
+    source = 'data/%s' % args.path
+    target = 'atviriduomenys.lt:/opt/atviriduomenys.lt/app/var/www/data/%s' % args.path
+    call(_rsync(args) + [source, target])
+
+
+def get(args):
+    source = 'atviriduomenys.lt:/opt/atviriduomenys.lt/app/var/www/data/%s' % args.path
+    target = 'data/%s' % args.path
+    call(_rsync(args) + [source, target])
 
 
 def main():
@@ -24,12 +45,30 @@ def main():
     subparsers = parser.add_subparsers()
 
     pars = subparsers.add_parser('push')
+    pars.add_argument('-n', '--dry-run', action='store_true', default=False,
+                      help='Do nothing, just show what would have been transferred.')
     pars.add_argument('--delete', action='store_true', default=False, help='Delete missing files from remote server.')
     pars.set_defaults(func=push)
 
     pars = subparsers.add_parser('pull')
+    pars.add_argument('-n', '--dry-run', action='store_true', default=False,
+                      help='Do nothing, just show what would have been transferred.')
     pars.add_argument('--delete', action='store_true', default=False, help='Delete missing files from local machine.')
     pars.set_defaults(func=pull)
+
+    pars = subparsers.add_parser('put')
+    pars.add_argument('path', default='', help='Path on local machine.')
+    pars.add_argument('-n', '--dry-run', action='store_true', default=False,
+                      help='Do nothing, just show what would have been transferred.')
+    pars.add_argument('--delete', action='store_true', default=False, help='Delete missing files from remote server.')
+    pars.set_defaults(func=put)
+
+    pars = subparsers.add_parser('get')
+    pars.add_argument('path', default='', help='Path on remote server.')
+    pars.add_argument('-n', '--dry-run', action='store_true', default=False,
+                      help='Do nothing, just show what would have been transferred.')
+    pars.add_argument('--delete', action='store_true', default=False, help='Delete missing files from local machine.')
+    pars.set_defaults(func=get)
 
     args = parser.parse_args()
     args.func(args)
