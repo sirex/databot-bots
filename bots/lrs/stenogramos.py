3347c213bb0139aca31a5d891e490e234ea1c319
Mantas <sirexas@gmail.com>
2017-01-28 15:50:44 +0200

    Update stenogramos scraper

--- bots/lrs/stenogramos.py
+++ bots/lrs/stenogramos.py
@@ -2,38 +2,7 @@
 
 import botlib
 
-from databot import this, select, task
-
-
-def define(bot):
-    bot.define('posėdžių-puslapiai', botlib.dburi('lrs/balsavimai'))
-    bot.define('stenogramų-sąrašas')
-    bot.define('stenogramų-puslapiai')
-    bot.define('metadata')
-
-
-def run(bot):
-    with bot.pipe('posėdžių-puslapiai'):
-        with bot.pipe('stenogramų-sąrašas').select([
-            '.fakt_pos ul.list > li xpath:a[text()="Stenograma"]/@href'
-        ]).dedup():
-            bot.pipe('stenogramų-puslapiai').download()
-
-    with bot.pipe('stenogramų-puslapiai'):
-        bot.pipe('metadata').select(this.key, select([
-            '.basic .ltb',
-            (select(':text').strip(), select('b:text?').strip()),
-        ]).cast(dict)).dedup()
-
-    bot.pipe('metadata').export('data/lrs/stenogramos/metadata.csv', include=[
-        'key',
-        'Data:',
-        'Rūšis:',
-        'Kalba:',
-        'Numeris:',
-    ])
-
-    bot.compact()
+from databot import define, select, task
 
 
 pipeline = {
@@ -67,4 +36,4 @@ pipeline = {
 
 
 if __name__ == '__main__':
-    botlib.runbot(define, run)
+    botlib.runbot(pipeline)
