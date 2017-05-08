b3f0fb4124b19666488175f96a85093a4ee3a8fe
Mantas <sirexas@gmail.com>
2017-05-08 21:47:50 +0300

    Refactor document scraper to declarative syntax

--- bots/lrs/dokumentai.py
+++ bots/lrs/dokumentai.py
@@ -1,54 +1,67 @@
 #!/usr/bin/env python3
 
+import yaml
 import botlib
 
-from databot import row, call, strip
+from databot import define, task, this, call, strip
 
 
-def define(bot):
-    bot.define('klausimų-puslapiai', botlib.dburi('lrs/posedziai'))
-    bot.define('dokumentų-sąrašas')
-    bot.define('dokumentų-puslapiai', compress=True)
-    bot.define('susijusių-dokumentų-sąrašas')
-    bot.define('susijusių-dokumentų-puslapiai', compress=True)
-    bot.define('metadata')
-    bot.define('texts')
+with open('settings.yml') as f:
+    settings = yaml.load(f)
 
+cookies = settings['cookies']['www.lrs.lt']
 
-def run(bot):
-    with bot.pipe('klausimų-puslapiai'):
-        with bot.pipe('dokumentų-sąrašas').select([
-            '#page-content div.default b xpath:a[text()="dokumento tekstas"]/@href'
-        ]).dedup():
-            bot.pipe('dokumentų-puslapiai').download()
+pipeline = {
+    'pipes': [
+        define('klausimų-puslapiai', botlib.dburi('lrs/posedziai')),
+        define('dokumentų-sąrašas'),
+        define('dokumentų-puslapiai', compress=True),
+        define('susijusių-dokumentų-sąrašas'),
+        define('susijusių-dokumentų-puslapiai', compress=True),
+        define('metadata'),
+        define('texts'),
+    ],
+    'tasks': [
+        task('klausimų-puslapiai', 'dokumentų-sąrašas').
+        select(['#page-content div.default b xpath:a[text()="dokumento tekstas"]/@href']).
+        dedup(),
 
-        with bot.pipe('susijusių-dokumentų-sąrašas').select([
-            '#page-content div.default b xpath:a[text()="susiję dokumentai"]/@href'
-        ]).dedup():
-            bot.pipe('susijusių-dokumentų-puslapiai').download()
+        task('dokumentų-sąrašas', 'dokumentų-puslapiai').
+        download(cookies=cookies, check='#page-content div.default b xpath:a[text()="dokumento tekstas"]'),
 
-    with bot.pipe('dokumentų-puslapiai'):
-        bot.pipe('metadata').select(row.key, call(dict, ['.basic .ltb', (strip(':text'), strip('b:text?'))])).dedup()
+        task('dokumentų-puslapiai', 'susijusių-dokumentų-sąrašas').
+        select(['#page-content div.default b xpath:a[text()="susiję dokumentai"]/@href']).
+        dedup(),
 
-    with bot.pipe('dokumentų-puslapiai'):
-        bot.pipe('texts').select(row.key, 'body > div:content').dedup()
+        task('susijusių-dokumentų-puslapiai').
+        download(cookies=cookies, check='#page-content div.default b xpath:a[text()="susiję dokumentai"]'),
 
-    bot.pipe('metadata').export('data/lrs/dokumentai/metadata.csv', include=[
-        'key',
-        'Data:',
-        'Rūšis:',
-        'Kalba:',
-        'Numeris:',
-        'Statusas:',
-    ])
+        task('dokumentų-puslapiai', 'metadata').
+        select(this.key, call(dict, ['.basic .ltb', (strip(':text'), strip('b:text?'))])).
+        dedup(),
 
-    bot.pipe('texts').export('data/lrs/dokumentai/texts.csv', include=[
-        'key',
-        'value',
-    ])
+        task('dokumentų-puslapiai', 'texts').
+        select(this.key, 'body > div:content').
+        dedup(),
 
-    bot.compact()
+        task('metadata').export('data/lrs/dokumentai/metadata.csv', include=[
+            'key',
+            'Data:',
+            'Rūšis:',
+            'Kalba:',
+            'Numeris:',
+            'Statusas:',
+        ]),
+
+        task('texts').export('data/lrs/dokumentai/texts.csv', include=[
+            'key',
+            'value',
+        ]),
+
+        task().compact(),
+    ],
+}
 
 
 if __name__ == '__main__':
-    botlib.runbot(define, run)
+    botlib.runbot(pipeline)
