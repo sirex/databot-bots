b6f4a5bdf9c841a3153cdb3e0a88377266e74980
Mantas <sirexas@gmail.com>
2017-05-11 16:55:44 +0300

    Fix voting scraper

--- bots/lrs/balsavimai.py
+++ bots/lrs/balsavimai.py
@@ -32,24 +32,24 @@ pipeline = {
         task('balsavimų-sąrašas', 'balsavimų-puslapiai').
         download(cookies=cookies, check='#page-content h1.page-title'),
 
-        task('klausimų-puslapiai', 'balsavimų-sąrašas').
+        task('balsavimų-puslapiai', 'balsavimų-duomenys').
         select(this.key, {
             'data': select('.page-title:text').re(r'\d{4}-\d{2}-\d{2}'),
             'posėdis': select('.page-title:text').re(r'(\w+) posėdis\)'),
             'klausimai': ['xpath://b/a[contains(@class, "link") and text()="dokumento tekstas"]', {
-                'pavadinimas': select('xpath:./../preceding-sibling::b/a[contains(@class, "link")]/text()'),
-                'rūšis': select('xpath:./../preceding-sibling::b[contains(a/@class, "link")]/following-sibling::text()[1]'),
-                'klausimo-nuoroda': select('xpath:./../preceding-sibling::b/a[contains(@class, "link")]/@href'),
+                'pavadinimas': select('xpath:./../preceding-sibling::b[contains(a/@class, "link")][1]/a/text()[1]'),
+                'rūšis': select('xpath:./../preceding-sibling::b[contains(a/@class, "link")][1]/following-sibling::text()[1]'),
+                'klausimo-nuoroda': select('xpath:./../preceding-sibling::b[contains(a/@class, "link")][1]/a/@href'),
                 'dokumento-nuoroda': select('@href'),
             }],
-            'formuluotė': select('#page-content .default xpath://text()[.="Formuluotė: "]/following-sibling::b[1]/text()'),
+            'formuluotė': select('#page-content .default xpath://text()[.="Formuluotė: "]/following-sibling::b[1]/text()?'),
             'laikas': select('#page-content .default xpath://text()[.="Balsavimo laikas: "]/following-sibling::b[1]/text()'),
             'balsavo-seimo-narių': select('#page-content .default xpath://text()[.="Balsavo Seimo narių: "]/following-sibling::b[1]/text()'),
             'viso-seimo-narių': select('#page-content .default xpath://text()[.="Balsavo Seimo narių: "]/following-sibling::text()[.=" iš "][1]/following-sibling::b[1]/text()'),
             'rezultatai': ['.sale-group-title xpath:text()[.="Individualūs balsavimo  rezultatai"]/../following-sibling::table/tr[count(td)=5]', {
                 'vardas': select('td[1] > a:text'),
                 'seimo-nario-nuoroda': select('td[1] > a@href'),
-                'p_asm_id': select('td[1] > a@href').urlparse().query.p_asm_id.cast(int),
+                'p_asm_id': select('td[1] > a@href').re('p_asm_id=(-?\d+)').cast(int),
                 'frakcija': select('td[2]:text'),
                 'už': select('td[3]:text'),
                 'prieš': select('td[4]:text'),
